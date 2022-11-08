558a1e8b977c8a9b87649a9588e6dea629454021
unknown <cogjava4341@iiht.tech>
2022-11-08 16:33:34 +0530

    Third commit Abstarct class

---
+++ src/Abstaction/CSK.java
@@ -0,0 +1,16 @@
+package Abstaction;
+
+public class CSK extends IPL {
+
+	@Override
+	void play() {
+		// TODO Auto-generated method stub
+		System.out.println("CSK is playing Good");
+		
+		}
+	public static void main(String[] args) {
+		CSK csk=new CSK();
+		csk.play();
+	}
+
+}
---
+++ src/Abstaction/IPL.java
@@ -0,0 +1,7 @@
+package Abstaction;
+
+public abstract class IPL {
+	
+	 abstract void play();
+
+}
