ff5b621ee44a000e2747151ae1a048ae62d9b7c7
unknown <cogjava4341@iiht.tech>
2022-11-10 14:02:00 +0530

    Calculate

---
+++ src/com/junit/demo/Calculate.java
@@ -0,0 +1,17 @@
+package com.junit.demo;
+
+public class Calculate {
+	public int add(int num1, int num2) {
+		int total=num1+num2;
+		return total;
+	}
+	public int sub(int num1, int num2) {
+		int total=num1-num2;
+		return total;
+	}
+	public int pro(int num1, int num2) {
+		int total=num1*num2;
+		return total;
+	}
+
+}
---
+++ src/com/junit/demo/CalculateUnit.java
@@ -0,0 +1,14 @@
+package com.junit.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculateUnit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
